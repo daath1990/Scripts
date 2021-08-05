cb65ffe3e20c7df2af40e23bbd3ae04d035fd71b
Xavier Mazon <xavi.mazon.bcn@gmail..com>
2021-08-05 17:43:16 +0200

    Puliendo bug de configEnviroment.sh

--- configEnviroment.sh
+++ configEnviroment.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Primero ejecutar el script en modo root y despues volver a ejectar el script usando el ZSH
+
 if [[ $EUID -eq 0 ]]; then
-    apt install gcc g++ emacs curl wget man python3 zsh vim fonts-powerline
+    apt install gcc g++ emacs curl wget man python3 zsh vim fonts-powerline -y
     chsh -s /usr/bin/zsh daath
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 else
     git config --global user.email "xavi.mazon.bcn@gmail.com"
     git config --global user.name "Xavier Mazon"
     git config --global init.defaultBranch main
     git clone https://github.com/xaviermazon/MyConfigEmacs
-    mv MyConfigEmacs .emacs.d
+    mv MyConfigEmacs ~/.emacs.d
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 fi
 
