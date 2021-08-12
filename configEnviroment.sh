db2c3fc84ae1fe4f498ebb739ef62a071f994cf9
Xavier Mazon <xavi.mazon.bcn@gmail.com>
2021-08-12 21:39:40 +0200

    Zsh eliminado, autocompletado Bash

--- configEnviroment.sh
+++ configEnviroment.sh
@@ -3,15 +3,14 @@
 # Primero ejecutar el script en modo root y despues volver a ejectar el script usando el ZSH
 
 if [[ $EUID -eq 0 ]]; then
-    apt install gcc g++ emacs curl wget man python3 zsh vim fonts-powerline -y
-    chsh -s /usr/bin/zsh daath
+    apt install gcc g++ emacs curl wget man python3 vim fonts-powerline -y
 else
     git config --global user.email "xavi.mazon.bcn@gmail.com"
     git config --global user.name "Xavier Mazon"
     git config --global init.defaultBranch main
     git clone https://github.com/xaviermazon/MyConfigEmacs
     mv MyConfigEmacs ~/.emacs.d
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+    echo "[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion" >> ~/.bash_profile
+    source ~/.bash_profile
 fi
 
